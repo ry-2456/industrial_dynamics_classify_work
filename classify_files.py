8d0d6047542fc160821f64cd52c02b2198853229
rintaro-takashiro <ittanmomennimakaretyaimashita@gmail.com>
2020-06-16 17:37:21 +0900

    add function to extract zipfile

--- classify_files.py
+++ classify_files.py
@@ -1,4 +1,5 @@
 import os
+import zipfile
 import shutil
 
 def get_studentid_and_name(f):
@@ -8,38 +9,53 @@ def get_studentid_and_name(f):
     name = ' '.join(l[1:-1]) + ' ' + l[-1].split("_")[0]
     return (student_id, name)
 
-def make_ans_dir(parent_dir, student_id, name):
+def make_ans_dir(parent_dir1, parent_dir2, student_id, name):
     "parent_dir以下にstdent_idとnameを基にディレクトリを作る"
     f1 = "{}_{}_1".format(student_id, name)
     f2 = "{}_{}_2".format(student_id, name)
 
-    if not os.path.exists(parent_dir + os.sep + f1):
-        os.mkdir(parent_dir + os.sep + f1)
+    if not os.path.exists(parent_dir1 + os.sep + f1):
+        os.mkdir(parent_dir1 + os.sep + f1)
 
-    if not os.path.exists(parent_dir + os.sep + f2):
-        os.mkdir(parent_dir + os.sep + f2)
+    if not os.path.exists(parent_dir2 + os.sep + f2):
+        os.mkdir(parent_dir2 + os.sep + f2)
+
+def extract_zip(path_to_zipfile, dst):
+    with zipfile.ZipFile(path_to_zipfile) as zip_file:
+        for info in zip_file.infolist():
+            info.filename = info.filename.encode("cp437").decode("cp932")
+            zip_file.extract(info, dst)
 
 if __name__ == "__main__":
-
-    print("###################################")
-    
     if not os.path.exists("question1"):
         os.mkdir("./" + os.sep + "question1")
     if not os.path.exists("question2"):
         os.mkdir("./" + os.sep + "question2")
 
     l = os.listdir(".")
+
     # ディレクトリだけをlに含める
     l = [d for d in l if "19T" in d]
-    for f_name in l:
-        print(get_studentid_and_name(f_name))
-    # for f in l:
-    #     make_ans_dir(f, *(get_studentid_and_name(f)))
-    
-    # ファイルが一つだけ存在する場合
-    # この場合にはそれをそれぞれのファイルをコピーする
+
+    # question1とquestion2以下に学生のフォルダを作る
+    for f in l:
+        make_ans_dir("question1", "question2", *(get_studentid_and_name(f)))
+
+    # zipファイルが存在すれば名前以下のフォルダに展開する展開する
+    for f in l:
+        zip_files = os.listdir(f)
+        zip_files = [f_name for f_name in zip_files if ".zip" in f_name]
+        if zip_files:
+            print(f)
+            try:
+                extract_zip(os.path.join(f, zip_files[0]), f)
+            except UnicodeEncodeError as e:
+                print("\n#########################")
+                print("文字コードの問題が発生した")
+                print("{}に関しては手動で行ってください".format(f))
+                print("#########################\n")
+
+    # ファイルをquestion1とquestion2以下にコピーする
 
     cwd = os.getcwd()
 
-
-    print("###################################")
